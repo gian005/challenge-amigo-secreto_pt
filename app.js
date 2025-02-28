e121c7f80e1dfcb44c9322e75874551ce5ef5d57
Gian Lucas Pereira Cunha <gianpereira526@gmail.com>
2025-02-27 23:14:44 -0300

    adicionar amigo

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let nomesDosAmigos = [];
+
+function adicionarAmigo(){
+    nomeDoAmigo = document.querySelector('input').value;
+    if (nomeDoAmigo == ""){
+        alert("Por favor, insira um nome.");
+    } else {
+        nomesDosAmigos .push(nomeDoAmigo);
+        console.log(nomesDosAmigos);
+        campo = document.querySelector('input');
+        campo.value = "";
+        exibirListaDeAmigos();
+    }
+}
+
+function exibirListaDeAmigos(){
+    lista2 = document.getElementById("listaAmigos");
+    console.log(lista2);
+    lista2.innerHTML = "";
+    lista2.innerHTML = nomesDosAmigos ;
+
+    // for(let x = 0; x < nomesDosAmigos.length;){
+    //    lista2.innerHTML = nomesDosAmigos ;
+    //    x ++;
+    // }
+}
+
+//adicionarAmigo();
