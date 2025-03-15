4e578c7a824c2536bcde8b35113bfd99304ae6fc
Gian Lucas Pereira Cunha <gianpereira526@gmail.com>
2025-03-14 22:13:45 -0300

    Criar button para limpar lista de amigos inseridos. Melhorar a regra de negócio da visualização dos nomes inseridos.

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 let amigos = [];
 let listaAmigos = document.getElementById("listaAmigos");
+let resultado = document.getElementById("resultado");
 
 function adicionarAmigo(){
+	resultado.innerHTML = "";
     nomeDoAmigo = document.querySelector('input').value;
     if (nomeDoAmigo == ""){
         alert("Por favor, insira um nome.");
     } else {
         amigos .push(nomeDoAmigo);
-        console.log(amigos);
         campo = document.querySelector('input');
         campo.value = "";
         exibirListaDeAmigos();
@@ -25,10 +26,26 @@ function exibirListaDeAmigos(){
 }
 
 function sortearAmigo(){
+	document.querySelector('.button-remove-name').removeAttribute('disabled');
 	if(amigos.length > 0){
 		let numeroSorteado = (Math.floor	(Math.random() * (amigos.length - 1 + 1)));
-		let resultado = document.getElementById("resultado");
+		let nomeSorteado = amigos[numeroSorteado];
 		listaAmigos.innerHTML = "";
-		resultado.innerHTML = amigos[numeroSorteado];
+		resultado.innerHTML = "O amigo secreto sorteado é: " + nomeSorteado;
+		}else {
+			alert("Não há nomes para sortear. Insira nomes no campo indicado.")
 		}
 	}
+	
+function novaListaDeAmigos(){
+	document.querySelector('.button-remove-name').setAttribute('disabled', true);
+	if(amigos.length > 0){
+	amigos = [];
+	listaAmigos.innerHTML = "";
+	resultado.innerHTML = "";
+		}else {
+			alert("Não há nomes na lista.")
+		}
+	}
+	
+
